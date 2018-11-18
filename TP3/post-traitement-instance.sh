4b6087b94edecfd2dcea27b7d048786955b49ce3
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2018-11-18 17:29:35 +0100

    pdf fusion

--- TP3/post-traitement-instance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-openstack server create --flavor m1.tiny --image centos7-xenial --security-group customSecurityGroup --key-name yonida --user-data post-userdata.sh post-traitement
-
--- TP3/povray-instance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-openstack container create povrayContainer
-openstack server create --flavor m1.tiny --image centos7-xenial --security-group customSecurityGroup --key-name yonida --user-data povray-userdata.sh  povray-instance
-
-
-
